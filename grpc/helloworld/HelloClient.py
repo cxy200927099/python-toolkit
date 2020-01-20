687806c21e277f4cee4cc83f161c2dedaf3692d2
chenxingyi <chenxingyi@xunlei.com>
2020-01-20 17:22:09 +0800

    python grpc demo

--- grpc/helloworld/HelloClient.py
+++ grpc/helloworld/HelloClient.py
@@ -3,3 +3,25 @@
 # @Time    : 2020/1/10 2:41 下午
 # @Author  : chenxingyi
 # @FileName: HelloClient.py
+
+import logging
+
+import grpc
+
+import hello_pb2
+import hello_pb2_grpc
+# from google.protobuf.wrappers_pb2 import StringValue
+
+def runClient():
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = hello_pb2_grpc.HelloWorldStub(channel)
+        person = hello_pb2.Person(name="cxy")
+        # msg = StringValue(value="greeter")
+        print("send msg")
+        # print(msg)
+        response = stub.SayHello(hello_pb2.ToBeGreeted(person=person, msg="greeter"))
+    print("client receive:%s" % response.message)
+
+
+if __name__ == '__main__':
+    runClient()
--- grpc/helloworld/HelloServer.py
+++ grpc/helloworld/HelloServer.py
@@ -3,3 +3,32 @@
 # @Time    : 2020/1/10 2:35 下午
 # @Author  : chenxingyi
 # @FileName: HelloServer.py
+
+from concurrent import futures
+import logging
+
+import grpc
+
+import hello_pb2
+import hello_pb2_grpc
+
+
+class Greeter(hello_pb2_grpc.HelloWorldServicer):
+
+    def SayHello(self, request, context):
+        print("receive %s msg:%s" %(request.person.name, request.msg))
+        return hello_pb2.Greeting(message="hi %s" % request.person.name)
+
+def startServer():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    hello_pb2_grpc.add_HelloWorldServicer_to_server(Greeter(), server)
+    ip_port = '[::]:50051'
+    print("start listen %s" % ip_port)
+    server.add_insecure_port(ip_port)
+    server.start()
+    print("server wait")
+    server.wait_for_termination()
+    print("server quit")
+
+if __name__ == '__main__':
+    startServer()
